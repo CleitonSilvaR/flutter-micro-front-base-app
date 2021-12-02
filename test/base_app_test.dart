50185744a44a247553f3d4794d9677e87548ba7e
Cleiton Silva Rego <cleiton_.98@hotmail.com>
2021-12-01 23:58:51 -0300

    adicionado sonar

---
+++ test/base_app_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('BaseAppTest Test', () {
+    test("First Test", () {
+      expect('TESTE', equals('TESTE'));
+    });
+  });
+}
